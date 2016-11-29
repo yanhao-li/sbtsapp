38c47dfa91e4c42ba22143c765f973efc0268eeb
maekks <hi@maxlee.im>
2016-11-29 16:58:58 -0500

    change build.sh file

---
+++ build.sh
@@ -0,0 +1,38 @@
+chmod 755 build
+cd build
+chmod 744 *.*
+chmod 755 WEB-INF
+chmod 755 resource
+cd WEB-INF
+chmod 744 *.*
+chmod 755 classes
+chmod 755 lib
+cd classes
+chmod 755 maxapp
+cd maxapp
+chmod 744 *.*
+pwd
+cd ..
+pwd
+cd ..
+pwd
+cd ..
+pwd
+cd resource
+pwd
+chmod 744 *.*
+pwd
+cd css
+chmod 744 *.*
+pwd
+cd ..
+cd images
+chmod 744 *.*
+cd ..
+pwd
+cd ..
+pwd
+cd ..
+pwd
+
+
